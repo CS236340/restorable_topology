b18424b2fc03630dc5e195407bd8510bcdf9a6be
louie-aw <louie.abuwarda@gmail.com>
2016-10-17 19:26:10 +0300

    Add files via upload

---
+++ src/mininet/customTopologyInitializerTest.py
@@ -0,0 +1,8 @@
+﻿#!/usr/bin/python
+
+import sys
+sys.path.append("/home/mininet/GUEST_SHARE/restorable_topology/src/mininet")
+
+from customTopologyInitializer import CustomTopologyInitializer
+
+topos = { 'customTopology': ( lambda: CustomTopologyInitializer("../../test_results/grid/grid1.p") ) }
